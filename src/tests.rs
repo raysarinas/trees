c0a3f9508e2c4566986fd1e7f514f8f79c87b4b7
raysarinas <raysarinas97@gmail.com>
2020-03-03 00:14:19 -0700

    clean up rbt tests (commented)

--- src/tests.rs
+++ src/tests.rs
@@ -77,134 +77,137 @@ pub fn benchmark_avl() {
     }
 }
 
-// pub fn it_works() {
-//     // Test TreeNode
-//     let treenode: red_black_tree::TreeNode<u32> = red_black_tree::TreeNode::new(5);
+// pub fn test_treenode() {
+//     let treenode: red_black_tree::RBTreeNode<u32> = red_black_tree::RBTreeNode::new(5);
         
-//     println!("{:?}", treenode.value());
-//     println!("{:?}", treenode.color());
+//     assert_eq!(treenode.value().unwrap(), 5);
+//     assert_eq!(treenode.color(), NodeColor::Red);
 
 //     treenode.set_color(red_black_tree::NodeColor::Black);
-//     println!("{:?}", treenode.color());
+//     assert_eq!(treenode.color(), NodeColor::Black);
 
 //     treenode.set_value(10);
-//     println!("{:?}", treenode.value());
+//     assert_eq!(treenode.value().unwrap(), 10);
 
+// }
 
-//     // Test RBTree
-//     println!("\n==== Start Testing RBTree Here ====\n");
+// fn test_count_leaves_rbt() {
+
+// }
+
+// fn test_height_rbt() {
+
+// }
+
+// fn test_size_rbt() {
+
+// }
+
+// pub fn test_insert_rbt_all_cases() {
 //     let mut rbt: red_black_tree::RBTree<u32> = red_black_tree::RBTree::new();
-//     println!("empty height = {}", rbt.height());
-//     println!("no leaves = {}", rbt.count_leaves());
-//     println!();
 
 //     let mut vec_in = vec![5, 70, 35, 8, 100, 60, 120, 1, 84, 17];
 //     for &x in vec_in.iter() {
-//         println!("Inserting {} ...", x);
 //         rbt.insert_node(x);
-//         println!("size = {}", rbt.size());
-//         rbt.print();
-//         println!("height = {}", rbt.height());
-//         println!("number of leaves = {}", rbt.count_leaves());
-//         println!();
-//     }
-//     vec_in = vec![35, 5, 1, 8, 17, 60, 70, 84, 100, 120];
-//     for &x in vec_in.iter() {
-//         assert!(rbt.search(x).is_some());
-//         assert!(rbt.contains(x) == true);
-//     }
-//     assert!(rbt.search(10).is_none());
-//     assert!(rbt.contains(10) == false);
-
-//     println!("\n==== Start Testing DELETE RBTree Here ====\n");
-//     let mut rbt2: red_black_tree::RBTree<u32> = red_black_tree::RBTree::new();
-//     let mut vec_in = vec![30, 20, 40, 10, 50];
-//     for &x in vec_in.iter() {
-//         println!("Inserting {} ...", x);
-//         rbt2.insert_node(x);
-//         println!("size = {}", rbt2.size());
-//         rbt2.print();
-//         println!("height = {}", rbt2.height());
-//         println!("leaves = {}", rbt2.count_leaves());
-//         println!();
 //     }
 
-//     vec_in = vec![10, 20, 30];
-//     for &x in vec_in.iter() {
-//         println!("Deleting {} ...", x);
-//         rbt2.delete_node(x);
-//         println!("size = {}", rbt2.size());
-//         rbt2.print();
-//         println!("height = {}", rbt2.height());
-//         println!("num leaves = {}", rbt2.count_leaves());
-//         println!(); 
-//     }
+//     // println!("\n==== Start Testing DELETE RBTree Here ====\n");
+//     // let mut rbt2: red_black_tree::RBTree<u32> = red_black_tree::RBTree::new();
+//     // let mut vec_in = vec![30, 20, 40, 10, 50];
+//     // for &x in vec_in.iter() {
+//     //     println!("Inserting {} ...", x);
+//     //     rbt2.insert_node(x);
+//     //     println!("size = {}", rbt2.size());
+//     //     rbt2.print();
+//     //     println!("height = {}", rbt2.height());
+//     //     println!("leaves = {}", rbt2.count_leaves());
+//     //     println!();
+//     // }
+
+//     // vec_in = vec![10, 20, 30];
+//     // for &x in vec_in.iter() {
+//     //     println!("Deleting {} ...", x);
+//     //     rbt2.delete_node(x);
+//     //     println!("size = {}", rbt2.size());
+//     //     rbt2.print();
+//     //     println!("height = {}", rbt2.height());
+//     //     println!("num leaves = {}", rbt2.count_leaves());
+//     //     println!(); 
+//     // }
 // }
 
-// pub fn test_delete_rbt() {
+// fn test_rbt_search_contains() {
 //     let mut rbt: red_black_tree::RBTree<u32> = red_black_tree::RBTree::new();
+//     let vec_in = vec![35, 5, 1, 8, 17, 60, 70, 84, 100, 120];
+
+//     for &x in vec_in.iter() {
+//         assert!(rbt.search(x).is_some());
+//         assert!(rbt.contains(x));
+//     }
+
+//     assert!(rbt.search(10).is_none());
+//     assert!(!rbt.contains(10));
+// }
+
+// fn test_delete_rbt_all() {
+//     let mut rbt: red_black_tree::RBTree<u32> = red_black_tree::RBTree::new();
+
+//     assert!(rbt.is_empty() == true);
 
 //     for x in 1..=10 {
 //         rbt.insert_node(x);
-//         println!("inserting ... {}", x);
+//         assert!(rbt.contains(x));
 //     }
 
 //     assert!(rbt.is_empty() == false);
 //     rbt.print();
 
-//     println!("deleting everything ..");
-
 //     let mut vec_in = vec![6, 4, 2, 8, 9, 7, 3, 1, 5, 10];
 //     for &x in vec_in.iter() {
-//         println!("deleting {} ...", x);
 //         rbt.delete_node(x);
+//         assert!(!rbt.contains(x));
 //     }
+
 //     assert!(rbt.is_empty() == true);
-//     rbt.print();
 // }
 
-// pub fn delete_cases_1_3_5_6() {
+
+// pub fn test_delete_rbt_cases_1_3_5_6() {
 //     let mut rbt: red_black_tree::RBTree<u32> = red_black_tree::RBTree::new();
     
+//     assert!(rbt.is_empty());
 //     for x in 1..=10 {
 //         rbt.insert_node(x);
-//         println!("inserting ... {}", x);
+//         assert!(rbt.contains(x));
 //     }
+//     assert!(!rbt.is_empty());
 
 //     let mut vec_in: Vec<u32> = vec![4, 6, 8, 1, 5, 9, 2];
 //     for &x in vec_in.iter() {
-//         println!("deleting ... {}", x);
 //         rbt.delete_node(x);
-//         rbt.print();
+//         assert!(!rbt.contains(x));
 //     }
+//     assert!(!rbt.is_empty());
 // }
 
-// pub fn delete_cases_2_4() {
-//     println!("\n==== Start Testing DELETE RBTree Here ====\n");
+// pub fn test_delete_rbt_cases_2_4() {
 
 //     let mut rbt: red_black_tree::RBTree<u32> = red_black_tree::RBTree::new();
 //     let mut vec_in: Vec<u32> = vec![5, 70, 35, 8, 98, 60, 99, 99, 1, 84, 17];
 
+//     assert!(rbt.is_empty());
 //     for &x in vec_in.iter() {
-//         println!("inserting {} ...", x);
 //         rbt.insert_node(x);
-//         println!("size is now = {}", rbt.size());
-//         rbt.print();
-//         println!("height = {}", rbt.height());
-//         println!("leaves = {}", rbt.count_leaves());
-//         println!();
+//         assert!(rbt.contains(x));
 //     }
+//     assert!(!rbt.is_empty());
 
-//     vec_in = vec![17, 84, 99, 5, 1, 60];
-//     for &x in vec_in.iter() {
-//         println!("deleting {} ...", x);
+//     let to_delete = vec![17, 84, 99, 5, 1, 60];
+//     for &x in to_delete.iter() {
 //         rbt.delete_node(x);
-//         println!("size is now = {}", rbt.size());
-//         rbt.print();
-//         println!("height = {}", rbt.height());
-//         println!("leaves = {}", rbt.count_leaves());
-//         println!();
+//         assert!(!rbt.contains(x));
 //     }
+//     assert!(!rbt.is_empty());
 // }
 
 // pub fn avl_test() {
